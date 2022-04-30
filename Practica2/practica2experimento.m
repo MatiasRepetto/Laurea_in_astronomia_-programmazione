5bd527d10d049340d02f213ee303f81ef974c321
Matias Repetto Zecchi <60527450+MatiasRepetto@users.noreply.github.com>
2022-04-30 04:40:06 -0300

    Add files via upload

---
+++ Practica2/practica2experimento.m
@@ -0,0 +1,25 @@
+clear;
+clc;
+
+Tref = 293;
+Rref = 1.30;
+alpha = 4.5e-03;
+k = physconst('Boltzmann');
+
+V = [12.00, 11.10, 10.30, 10.00, 9.10, 8.60, 8.30, 7.80, 7.30, 7.00, 6.10, 5.60];
+I = [1.74, 1.67, 1.60, 1.58, 1.50, 1.46, 1.43, 1.39, 1.34, 1.31, 1.23, 1.18];
+Vout = [169.9, 147.2, 130.8, 124.3, 105.6, 95.7, 40.1, 82.1, 72.8, 67.8, 53.2, 45.8];
+
+T = ((I-Rref)./(alpha*Rref))+Tref;
+
+LgVs = log10(Vout);
+LgKT = log10(k*(T.^(12)));
+p = polyfit(LgVs, LgKT, 2);
+j = (p(1)*(LgVs.^(2)))+(p(2)*LgVs)+p(3);
+
+hold on
+scatter(LgVs, LgKT);
+plot(LgVs, j);
+
+
+%Have to Fix it
